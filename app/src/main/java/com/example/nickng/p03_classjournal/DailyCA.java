0fcbf20c4d562d26f0860d2ddfa97e411d9e36f4
nickngweipeng <15056201@myrp.edu.sg>
2017-05-04 12:03:34 +0800

    Updated: Nick

--- app/src/main/java/com/example/nickng/p03_classjournal/DailyCA.java
+++ app/src/main/java/com/example/nickng/p03_classjournal/DailyCA.java
@@ -3,7 +3,7 @@ package com.example.nickng.p03_classjournal;
 import java.io.Serializable;
 
 /**
- * Created by 15056201 on 4/5/2017.
+ * Created by 15056201 on 4/5/2017..
  */
 
 public class DailyCA implements Serializable{
