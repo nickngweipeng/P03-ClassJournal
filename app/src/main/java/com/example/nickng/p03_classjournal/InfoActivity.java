1c506468ce589fdaf47c1cf397af7313457e5a18
RX101 <15041867@myrp.edu.sg>
2017-05-04 13:20:47 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_info.xml

--- app/src/main/java/com/example/nickng/p03_classjournal/InfoActivity.java
+++ app/src/main/java/com/example/nickng/p03_classjournal/InfoActivity.java
@@ -5,9 +5,17 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
+
+    ListView lv1;
+    ArrayAdapter aa1;
+    ArrayList<DailyCA> dailyCA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,16 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
         Button btnRP = (Button)findViewById(R.id.buttonInfo);
         Button btnAdd = (Button)findViewById(R.id.buttonAdd);
+        lv1 = (ListView) this.findViewById(R.id.listViewInfo);
+
+        Intent i = getIntent();
+        String types = i.getStringExtra("class");
+        dailyCA = new ArrayList<DailyCA>();
+
+        // Link this Activity object, the row.xml layout for
+        //  each row and the food String array together
+        aa1 = new InfoAdapter(this, R.layout.row, dailyCA);
+        lv1.setAdapter(aa1);
 
         btnRP.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +50,7 @@ public class InfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent rpIntent = new Intent(InfoActivity.this, addGrade.class);
-                rpIntent.putExtra("pos", String.valueOf(.size()+1);
+                rpIntent.putExtra("pos", String.valueOf(dailyCA.size()+1));
                 startActivity(rpIntent);
             }
         });
