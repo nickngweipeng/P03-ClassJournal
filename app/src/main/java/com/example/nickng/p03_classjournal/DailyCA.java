c9af95bf2b05e18a92b1ae83317263567a7ccc2d
nickngweipeng <15056201@myrp.edu.sg>
2017-05-04 11:48:09 +0800

    Updated: Nick

---
+++ app/src/main/java/com/example/nickng/p03_classjournal/DailyCA.java
@@ -0,0 +1,25 @@
+package com.example.nickng.p03_classjournal;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15056201 on 4/5/2017.
+ */
+
+public class DailyCA implements Serializable{
+    private String week;
+    private String grade;
+
+    public String getWeek() {
+        return week;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public DailyCA(String week, String grade) {
+        this.week = week;
+        this.grade = grade;
+    }
+}
--- app/src/main/java/com/example/nickng/p03_classjournal/MainActivity.java
+++ app/src/main/java/com/example/nickng/p03_classjournal/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.nickng.p03_classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClass = (Button)findViewById(R.id.btnClass);
+
+        btnClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
