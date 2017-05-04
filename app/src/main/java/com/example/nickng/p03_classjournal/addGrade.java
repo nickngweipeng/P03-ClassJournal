34fddbf8110e8597e02b401c757ff23c6305da89
weirdo2809 <15035648@myrp.edu.sg>
2017-05-04 12:27:17 +0800

    Hamzah

--- app/src/main/java/com/example/nickng/p03_classjournal/addGrade.java
+++ app/src/main/java/com/example/nickng/p03_classjournal/addGrade.java
@@ -9,5 +9,6 @@ public class addGrade extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grade);
+        String pos = getIntent().getStringExtra("pos");
     }
 }
