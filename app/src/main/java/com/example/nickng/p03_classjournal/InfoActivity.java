a7135f8dd2f20e457e132a78ebc35fa3b8b19f74
15056130 <15056130@myrp.edu.sg>
2017-05-04 13:29:44 +0800

    commit

--- app/src/main/java/com/example/nickng/p03_classjournal/InfoActivity.java
+++ app/src/main/java/com/example/nickng/p03_classjournal/InfoActivity.java
@@ -15,6 +15,7 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
         Button btnRP = (Button)findViewById(R.id.buttonInfo);
         Button btnAdd = (Button)findViewById(R.id.buttonAdd);
+        Button btnEmail = (Button)findViewById(R.id.buttonEmail);
 
         btnRP.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +35,32 @@ public class InfoActivity extends AppCompatActivity {
                 Intent rpIntent = new Intent(InfoActivity.this, addGrade.class);
                 rpIntent.putExtra("pos", String.valueOf(.size()+1);
                 startActivity(rpIntent);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String to = "jackielim8695@gmail.com";
+                String subject = "C347";
+                String message = "Hi Faci," +
+                        "I am ......." +
+                        "Please see my remarks so far, Thank You" +
+                        "";
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{ to});
+                //email.putExtra(Intent.EXTRA_CC, new String[]{ to});
+                //email.putExtra(Intent.EXTRA_BCC, new String[]{to});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+
+                //need this to prompts email client only
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+
             }
         });
     }
