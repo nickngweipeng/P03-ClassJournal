722b207dfd467042f70bee536df5af958482e563
weirdo2809 <15035648@myrp.edu.sg>
2017-05-04 12:25:11 +0800

    Hamzah

--- app/src/main/java/com/example/nickng/p03_classjournal/InfoActivity.java
+++ app/src/main/java/com/example/nickng/p03_classjournal/InfoActivity.java
@@ -1,7 +1,11 @@
 package com.example.nickng.p03_classjournal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        Button btnRP = (Button)findViewById(R.id.buttonInfo);
+        Button btnAdd = (Button)findViewById(R.id.buttonAdd);
+
+        btnRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rpIntent = new Intent(InfoActivity.this, addGrade.class);
+                rpIntent.putExtra("pos", String.valueOf(size()+1);
+                startActivity(rpIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/nickng/p03_classjournal/addGrade.java
@@ -0,0 +1,13 @@
+package com.example.nickng.p03_classjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addGrade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_grade);
+    }
+}
